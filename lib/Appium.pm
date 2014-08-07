7421cc10ad7915e502af29b65218b7ac1025fb65
Daniel Gempesaw <gempesaw@gmail.com>
2014-08-07 18:04:16 -0400

    Coerce desired capabilities with Appium defaults

--- lib/Appium.pm
+++ lib/Appium.pm
@@ -4,4 +4,42 @@ package Appium;
 
 # ABSTRACT: Perl bindings to the Appium mobile automation framework
 
+package Appium;
+use Moo;
+use Carp qw/croak/;
+extends 'Selenium::Remote::Driver';
+
+has '+desired_capabilities' => (
+    is => 'rw',
+    required => 1,
+    coerce => sub {
+        my $caps = shift;
+        croak 'Desired Capabilities must include: app' unless exists $caps->{app};
+
+        my $defaults = {
+            browserName => '',
+            deviceName => 'iPhone Simulator',
+            platformName => 'iOS',
+            platformVersion => '7.1'
+        };
+
+        foreach (keys %$defaults) {
+            unless (exists $caps->{$_}) {
+                $caps->{$_} = $defaults->{$_};
+            }
+        }
+
+        return $caps;
+    }
+);
+
+has 'port' => (
+    is => 'rw',
+    default => sub { 4723 }
+);
+
+has 'commands' => (
+    is => 'ro',
+    default => sub { Appium::Commands->new }
+);
 1;
