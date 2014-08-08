e15f37959d80b7222828ca167bcee94bb7518551
Daniel Gempesaw <gempesaw@gmail.com>
2014-08-08 10:43:44 -0400

    Fix syntactical errors in command schema

--- lib/Appium/Commands.pm
+++ lib/Appium/Commands.pm
@@ -21,167 +21,167 @@ has 'get_cmds' => (
         my $appium_commands = {
             contexts => {
                 method => 'GET',
-                url => '/session/$sessionId/contexts',
+                url => 'session/:sessionId/contexts',
                 no_content_success => 0
             },
             get_current_context => {
                 method => 'GET',
-                url => '/session/$sessionId/context',
+                url => 'session/:sessionId/context',
                 no_content_success => 0
             },
             switch_to_context => {
                 method => 'POST',
-                url => '/session/$sessionId/context',
+                url => 'session/:sessionId/context',
                 no_content_success => 1
             },
             # touch_action => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/touch/perform',
+            #         url => 'session/:sessionId/touch/perform',
             #         no_content_success => 1
             #     },
             #     multi_action => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/touch/multi/perform',
+            #         url => 'session/:sessionId/touch/multi/perform',
             #         no_content_success => 1
             #     },
             #     get_app_strings => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/strings',
+            #         url => 'session/:sessionId/appium/app/strings',
             #         no_content_success => 1
             #     },
             #     press_keycode => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/press_keycode',
+            #         url => 'session/:sessionId/appium/device/press_keycode',
             #         no_content_success => 1
             #     },
             #     long_press_keycode => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/long_press_keycode',
+            #         url => 'session/:sessionId/appium/device/long_press_keycode',
             #         no_content_success => 1
             #     },
             #     get_current_activity => {
             #         method => 'GET',
-            #         url => '/session/$sessionId/appium/device/current_activity',
+            #         url => 'session/:sessionId/appium/device/current_activity',
             #         no_content_success => 0
             #     },
             #     set_immediate_value => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/element/$elementId/value',
+            #         url => 'session/:sessionId/appium/element/$elementId/value',
             #         no_content_success => 1
             #     },
             #     pull_file => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/pull_file',
+            #         url => 'session/:sessionId/appium/device/pull_file',
             #         no_content_success => 1
             #     },
             #     pull_folder => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/pull_folder',
+            #         url => 'session/:sessionId/appium/device/pull_folder',
             #         no_content_success => 1
             #     },
             #     push_file => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/push_file',
+            #         url => 'session/:sessionId/appium/device/push_file',
             #         no_content_success => 1
             #     },
             #     complex_find => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/complex_find',
+            #         url => 'session/:sessionId/appium/app/complex_find',
             #         no_content_success => 1
             #     },
             #     background => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/background',
+            #         url => 'session/:sessionId/appium/app/background',
             #         no_content_success => 1
             #     },
             #     is_app_installed => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/app_installed',
+            #         url => 'session/:sessionId/appium/device/app_installed',
             #         no_content_success => 1
             #     },
             #     install_app => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/install_app',
+            #         url => 'session/:sessionId/appium/device/install_app',
             #         no_content_success => 1
             #     },
             #     remove_app => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/remove_app',
+            #         url => 'session/:sessionId/appium/device/remove_app',
             #         no_content_success => 1
             #     },
             #     launch_app => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/launch',
+            #         url => 'session/:sessionId/appium/app/launch',
             #         no_content_success => 1
             #     },
             #     close_app => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/close',
+            #         url => 'session/:sessionId/appium/app/close',
             #         no_content_success => 1
             #     },
             #     end_test_coverage => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/end_test_coverage',
+            #         url => 'session/:sessionId/appium/app/end_test_coverage',
             #         no_content_success => 1
             #     },
             #     lock => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/lock',
+            #         url => 'session/:sessionId/appium/device/lock',
             #         no_content_success => 1
             #     },
             #     shake => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/shake',
+            #         url => 'session/:sessionId/appium/device/shake',
             #         no_content_success => 1
             #     },
             #     reset => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/app/reset',
+            #         url => 'session/:sessionId/appium/app/reset',
             #         no_content_success => 1
             #     },
             hide_keyboard => {
                 method => 'POST',
-                url => '/session/$sessionId/appium/device/hide_keyboard',
+                url => 'session/:sessionId/appium/device/hide_keyboard',
                 no_content_success => 1
             },
             #     open_notifications => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/appium/device/open_notifications',
+            #         url => 'session/:sessionId/appium/device/open_notifications',
             #         no_content_success => 1
             #     },
             #     get_network_connection => {
             #         method => 'GET',
-            #         url => '/session/$sessionId/network_connection',
+            #         url => 'session/:sessionId/network_connection',
             #         no_content_success => 0
             #     },
             #     set_network_connection => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/network_connection',
+            #         url => 'session/:sessionId/network_connection',
             #         no_content_success => 1
             #     },
             #     get_available_ime_engines => {
             #         method => 'GET',
-            #         url => '/session/$sessionId/ime/available_engines',
+            #         url => 'session/:sessionId/ime/available_engines',
             #         no_content_success => 0
             #     },
             #     is_ime_active => {
             #         method => 'GET',
-            #         url => '/session/$sessionId/ime/activated',
+            #         url => 'session/:sessionId/ime/activated',
             #         no_content_success => 0
             #     },
             #     activate_ime_engine => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/ime/activate',
+            #         url => 'session/:sessionId/ime/activate',
             #         no_content_success => 1
             #     },
             #     deactivate_ime_engine => {
             #         method => 'POST',
-            #         url => '/session/$sessionId/ime/deactivate',
+            #         url => 'session/:sessionId/ime/deactivate',
             #         no_content_success => 1
             #     },
             #     get_active_ime_engine => {
             #         method => 'GET',
-            #         url => '/session/$sessionId/ime/active_engine',
+            #         url => 'session/:sessionId/ime/active_engine',
             #         no_content_success => 0
             #     }
         };
