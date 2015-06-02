4971eba0cf0b4d5e49f6685fe3954259040a9ab6
Daniel Gempesaw <gempesaw@gmail.com>
2015-06-01 21:33:24 -0400

    Move page into its own role

--- lib/Appium.pm
+++ lib/Appium.pm
@@ -724,92 +724,6 @@ information.
 
 =cut
 
-=method page
-
-A shadow of L<arc|https://github.com/appium/ruby_console>'s page
-command, this will print to STDOUT a list of all the visible elements
-on the page along with whatever details are available (name, label,
-value, etc). It's currently only compatible with iOS, and it doesn't
-take filtering arguments like arc's version of page does.
-
-    $appium->page;
-    # UIAWindow
-    #   UIATextField
-    #     name          : IntegerA
-    #     label         : TextField1
-    #     value         : 5
-    #     UIATextField
-    #       name        : TextField1
-    #       label       : TextField1
-    #       value       : 5
-    #   UIATextField
-    #     name          : IntegerB
-    #     label         : TextField2
-    #     UIATextField
-    #       name        : TextField2
-    #       label       : TextField2
-    # ...
-
-=cut
-
-sub page {
-    my ($self) = @_;
-
-    $self->_get_page;
-}
-
-sub _get_page {
-    my ($self, $element, $level) = @_;
-    return 'TODO: implement page on android' if $self->is_android;
-
-    $element //= $self->_source_window_with_children;
-    $level //= 0;
-    my $indent = '  ' x $level;
-
-    # App strings are found in an actual file in the app package
-    # somewhere, so I'm assuming we don't have to worry about them
-    # changing in the middle of our app execution. This may very well
-    # turn out to be a false assumption.
-    state $strings = $self->app_strings;
-
-    my @details = qw/name label value hint/;
-    if ($element->{visible}) {
-        print $indent .  $element->{type} . "\n";
-        foreach (@details) {
-            my $detail = $element->{$_};
-            if ($detail) {
-                print $indent .  '  ' . $_ . "\t: " . $detail  . "\n" ;
-
-                foreach my $key (keys %{ $strings }) {
-                    my $val = $strings->{$key};
-                    if ($val =~ /$detail/) {
-                        print $indent .  '  id  ' . "\t: " . $key . ' => ' . $val . "\n";
-                    }
-                }
-            }
-        }
-    }
-
-    $level++;
-    my @children = @{ $element->{children} };
-    foreach (@children) {
-        $self->_get_page($_, $level);
-    }
-}
-
-sub _source_window_with_children {
-    my ($self, $index) = @_;
-    $index //= 0;
-
-    my $window = $self->execute_script('UIATarget.localTarget().frontMostApp().windows()[' . $index . '].getTree()');
-    if (scalar @{ $window->{children} }) {
-        return $window;
-    }
-    else {
-        return $self->_source_window_with_children(++$index);
-    }
-}
-
 sub is_android {
     return shift->_type eq 'Android'
 }
@@ -817,6 +731,16 @@ sub is_android {
 sub is_ios {
     return shift->_type eq 'iOS'
 }
+
+=method page
+
+Display a quick summary of all elements that Appium thinks it can
+interact with. See L<Appium::CanPage/page> for more information.
+
+=cut
+
+with 'Appium::CanPage';
+
 
 =head1 SEE ALSO
 
