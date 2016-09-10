df543e527f47995999efc7a1aa2f5c29992c12ca
Daniel Gempesaw <gempesaw@gmail.com>
2016-09-10 10:30:47 -0400

    Begin TouchActions refactor with tap subroutine

--- lib/Appium/TouchActions.pm
+++ lib/Appium/TouchActions.pm
@@ -2,59 +2,78 @@ package Appium::TouchActions;
 
 # ABSTRACT: Perform touch actions through appium: taps, swipes, scrolling
 use Moo;
+use Scalar::Util qw/blessed/;
 
 has 'driver' => (
     is => 'ro',
     required => 1
-    
 );
 
-=method tap ( $x, $y )
+has 'actions' => (
+	is => 'lazy',
+	default => sub { [] },
+	clearer => 1
+);
 
-Perform a precise tap at a certain location on the device, specified
-either by pixels or percentages. All values are relative to the top
-left of the device - by percentages, (0,0) would be the top left, and
-(1, 1) would be the bottom right.
+=head1 SYNOPSIS
 
-As per the Appium documentation, values between 0 and 1 will be
-interepreted as percentages. (0.5, 0.5) will click in the center of
-the screen. Values greater than 1 will be interpreted as pixels. (10,
-10) will click at ten pixels away from the top and left edges of the
-screen.
+    my $appium = Appium->new;
+    my $elem = Appium->find_element('tap_me', 'id');
 
-    # tap in the center of the screen
-    $appium->tap( 0.5, 0.5 )->perform();
+    my $ta = TouchActions->new(driver => $appium);
+    $ta->tap($elem, 10, 10);
+    $ta->press(50, 50)
+      ->move_to(50, 50)
+      ->release;
+    $ta->perform; # taps $elem at (10, 10) from its top let corner, then
+                  # presses at (50, 50), slides to (100, 100), and
+                  # releases.
 
-    # tap a pixel position
-    $appium->tap( 300, 500 )->perform();
+=head1 DESCRIPTION
+
+The TouchActions class is used for performing more complicated actions
+on a mobile device. This class must be constructed separately from the
+Appium driver class itself, and it takes the driver as an argument
+during construction.
+
+When constructing a TouchAction chain, none of the actions are
+evaluated until you call L</perform>.
+
+The majority of the methods in this class take at least four optional
+arguments, in this specific order: C<[element], [x_coord], [y_coord],
+[duration]>.
+
+=for :list
+
+* element (optional) - if specified, this must be an
+  Appium::WebElement, and it must be the first item in the list
+
+* x, y (optional) - accepts values in pixels. if either of (x, y) is
+  specified, the other must be as well. It doesn't make sense to only
+  specify one of the two. If C<element> is also specified, the
+  location will be relative to the top left corner of the specified
+  element. If C<element> is not specified, the location will be
+  relative to the top left corner of the entire device
+
+* duration (optional) - some subroutines accept an millisecond amount
+  of time to perform the action. For example, the different between
+  L</press> and L</long_press> is the duration of time specified.
+
+=method tap ( [$element], [$x, $y] )
+
+Perform a tap at a certain location on the device. The arguments are
+C<[$element], [$x], $[y]>; duration is not an option here. For a tap
+with duration, see L</long_press>.
 
 =cut
 
 sub tap {
-    my ($self, @coords) = @_;
+    my ($self, @args) = @_;
 
-	my $coordinates = {
-        x => $coords[0],
-        y => $coords[1]
-    };
-
-	my $params = { action => 'tap' , options => $coordinates };
-	push @{$self->{'perform_actions'}}, $params;
-    return $self->driver;
+	my $options = $self->_parse_opts(@args);
+	$self->_add_action('tap', $options);
+    return $self;
 }
-
-=method press ( $x, $y )
-
-Perform a precise press at a certain location on the device, specified
-either by pixels or percentages. All values are relative to the top
-left of the device - by percentages, (0,0) would be the top left, and
-(1, 1) would be the bottom right.
-
-As per the Appium documentation, values between 0 and 1 will be
-interepreted as percentages. (0.5, 0.5) will click in the center of
-the screen. Values greater than 1 will be interpreted as pixels. (10,
-10) will click at ten pixels away from the top and left edges of the
-screen.
 
     # press in the center of the screen
     $appium->press( 0.5, 0.5 )->perform();
@@ -210,6 +229,52 @@ sub swipe {
 	  ->wait($coords[4])
 	  ->move_to($coords[2], $coords[3])
 	  ->release;
+}
+
+sub _add_action {
+	my ($self, $action, $options) = @_;
+
+	my $gesture = {
+		action => $action,
+		options => $options
+	};
+
+	$self->actions(push($self->actions, $gesture));
+}
+
+=pod _parse_opts
+
+The TouchAction methods all take optional arguments in the following
+order: C<element, x_coord, y_coord, duration>. Since all of the
+options are optional, there is some finagling to do to construct the
+proper <opts> argument that Appium expects.
+
+=cut
+
+sub _parse_opts {
+	my ($self) = shift;
+	my $opts = {};
+
+	my $element = shift || undef;
+	if (blessed($element) && $element->isa('Appium::WebElement')) {
+		$opts->{element} = $element->id;
+	}
+
+	my $x = shift || undef;
+	my $y = shift || undef;
+	if ($x == undef or $y == undef) {
+		# It doesn't make sense to specify only an $x, or only a $y.
+		($x, $y) = (undef, undef);
+	}
+	$opts->{x} = $x;
+	$opts->{y} = $y;
+
+	my $duration = shift || undef;
+	if ($duration != undef) {
+		$opts->{duration} = $duration;
+	}
+
+	return $opts;
 }
 
 1;
